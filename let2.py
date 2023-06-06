e234c5dba7d00b16ef7461198a011a782925a634
skat slim <tester0521@MacBook-Pro-skat.local>
2023-06-06 19:56:21 +0300

    let2 added

---
+++ let2.py
@@ -0,0 +1,30 @@
+from itertools import *
+
+
+
+
+def let2() -> print:
+    # №18808
+    # f = lambda x, y, z, w: (x and not(y)) or (y == z) or w
+    # ultra = [[[x1, x2, x3, 1], [1, x4, x5, x6], [1, 1, x7, x8]] for x1, x2, x3, x4, x5, x6, x7, x8 in product([0, 1], repeat = 8)]
+
+    # 18071
+    # f = lambda x, y, z, w: (x and not(y)) or (y == z) or not(w)
+    # ultra = [[[0, x1, x2, 0], [0, 1, 0, 1], [x3, 1, 0, x4]] for x1, x2, x3, x4 in product([0, 1], repeat = 4)]
+
+    # 27531
+    # f = lambda x, y, z, w: not((x <= y) and (y != z) and (z or w))
+    # ultra = [[[1, 1, x1, 1], [x2, 1, 1, x3], [1, x4, x5, x6]] for x1, x2, x3, x4, x5, x6 in product([0, 1], repeat = 6)]
+
+    # 16878
+    f = lambda x, y, z, w: (x != y) <= (z == (y or w))
+    ultra = [[[0, x1, 0, x2], [0, 0, x3, 0], [0, x4, x5, 0]] for x1, x2, x3, x4, x5 in product([0,1], repeat = 5)]
+
+    for el in ultra:
+        positions = [(x, y, z, w) for x, y, z, w in permutations(range(4)) if all(f(row[x], row[y], row[z], row[w]) == False for row in el)]
+        if len(positions) == 1:
+            (x, y, z, w) = positions[0]
+            print(f'x: {x} y: {y} z: {z} w: {w}')
+let2()
+
+
--- let25.py
+++ let25.py
@@ -1,2 +1,49 @@
+# 28120
+
+
+def kill_imposters(arr: list) -> list:
+    imposters = []
+    for el in arr:
+        c = 0
+        for el2 in arr:
+            if el % el2 == 0: c += 1
+        if c > 1:
+            imposters.append(el)
+    arr = list(filter(lambda x: x not in imposters, arr))
+    return arr
+
+
+
+def _1() -> print:
+    arr = [i for i in range(201455, 201471)]
+    divs = [i for i in range(2, int(201470**0.5) + 1)] # kill_imposters([i for i in range(2, int(201470**0.5) + 1)])
+    print(divs)
+    for el in arr:
+        test = []
+        for el2 in divs:
+            if len(test) > 2: break
+            elif el % el2 == 0:
+                test.append(el2)
+                test.append(el // el2)
+        if len(test) == 2:
+            test.append(el)
+            test.append(1)
+            print(sorted(test))
+
+
+# 33527
+
+def def2() -> print:
+    divs, gg = kill_imposters([i for i in range(2, int(102_000_000**0.5)+ 1, 2)]), [x for x in range(101_000_000, 102_000_001, 2) if int(x**0.5) * int(x**0.5) == x]
+    for x in gg:
+        for y in divs:
+                if (x % y == 0) and (y not in [2, int(x**0.5), x]): break
+        print(x)
+
+
+
+def2()
+
+
 
 
